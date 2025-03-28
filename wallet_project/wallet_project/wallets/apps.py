65c58906352a3c16f0624a433a6e5d7fa848d372
Evgeniy <aeon.kuzin@gmail.com>
2025-03-28 22:45:12 +0300

    Delete wallet_project/wallet_project directory

--- wallet_project/wallet_project/wallets/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class WalletsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'wallets'
-    verbose_name = "Управление кошельками"  # Человеко-понятное имя для админки
--- wallet_project/wallet_project/wallets/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import WalletBalanceView, WalletOperationView
-
-urlpatterns = [
-    path("api/v1/wallets/<uuid:wallet_id>/", WalletBalanceView.as_view(), name="wallet-balance"),
-    path("api/v1/wallets/<uuid:wallet_id>/operation/", WalletOperationView.as_view(), name="wallet-operation"),
-]
